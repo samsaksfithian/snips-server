ee7c2b43a715690f4ccd55a183fd48bca1b10f99
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-08-20 11:31:26 -0700

    added cors and default port

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const { logger, router, errorHandler } = require('./middleware');
 
 const app = express();
 
 /* Middleware */
+app.use(cors());
 app.use(express.json()); // parses requests with JSON payloads
 app.use(logger);
 app.use(router);
 app.use(errorHandler);
 
 /* Now start our app */
-app.listen(process.env.PORT, () => {
-  console.log(`Snips server running on port ${process.env.PORT}`);
+const default_port = 5555;
+app.listen(process.env.PORT || default_port, () => {
+  console.log(`Snips server running on port ${process.env.PORT || default_port}`);
 });
